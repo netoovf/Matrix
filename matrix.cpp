9029e886430fc474e66ef600c0439b03c263e69d
igorfel <igor_colgate@hotmail.com>
2014-09-24 16:26:43 -0300

    função para alocação

--- matrix.cpp
+++ matrix.cpp
@@ -1,7 +1,25 @@
-#include "matrix.h"
+#include "Matrix.h"
+#include "iostream"
 
 Matrix::Matrix()
 {
     this->rows = 0;
     this->cols = 0;
+}
+
+void Matrix::Alloc(unsigned rows, unsigned cols)
+{
+    try
+    {
+        this->Mat = new double*[rows];
+        for(unsigned i = 0; i < rows; i++)
+            Mat[i] = new double[cols];
+
+        this->rows = rows;
+        this->cols = cols;
+    }
+    catch(std::bad_alloc&)
+    {
+        std::cout << "erro na alocação";
+    }
 }
